e7c8f0f3b95b385747bb2c40a50ffad27f08a97b
amlanxbghn <amlanxconnect@gmail.com>
2024-07-13 11:19:38 +0530

    push

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
+
 import "./App.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import ProtectedRoute from "./components/protectedRoute";
 
 
 function App() {
@@ -17,7 +19,7 @@ function App() {
     
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/" element={<Home/>} />
+          <Route path="/" element={<ProtectedRoute><Home/></ProtectedRoute>} />
          
         </Routes>
       </BrowserRouter>
